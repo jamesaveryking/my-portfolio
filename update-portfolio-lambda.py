14abf462976c4b494711fde05bbc5a71cb219995
jamesaveryking <jamesaveryking@gmail.com>
2018-05-11 18:00:48 -0400

    new version of lambda function

--- update-portfolio-lambda.py
+++ update-portfolio-lambda.py
@@ -1,15 +1,20 @@
 import boto3
-s3 = boto3.resource('s3')
-portfolio_bucket = s3.Bucket('portfolio.jamesaveryking.com')
-for obj in portfolio_bucket.objects.all():
-     print (obj.key)
-portfolio_bucket.download_file('index.html', '/tmp/index.html')
-build_bucket = s3.Bucket('portfoliobuild.jamesaveryking.com')
-build_bucket.download_file('portfoliobuild.zip', '/tmp/portfoliobuild.zip')
-from io import StringIO
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+from botocore.client import Config
+import StringIO
 import zipfile
+import mimetypes
+
+s3 = bot3.resource('s3', config = Config(signature_version='s3v4'))
+
+portfolio_bucket = s3.Bucket('portfolio.jamesaveryking.com')
+build_bucket = s3.Bucket('portfoliobuild.jamesaveryking.com')
+
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfoio_zip)
+
 with zipfile.ZipFile(portfolio_zip) as myzip:
     for nm in myzip.namelist():
-        print (nm)
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm,
+            ExtraArgs ={'ContentType':mimetypes.guess_type{nm}[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
