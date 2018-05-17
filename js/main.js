482abc86990039d00f7c3fb35ba8a25cd1e71d5d
jamesaveryking <jamesaveryking@gmail.com>
2018-05-17 00:05:48 -0400

    Done for the night

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work.js';
 
-console.log("Loaded react-dom");
-
-console.log("Webpack works");
+const myWork = [
+  {
+    'title': "Coding Examples",
+    'image': {
+      'desc': "Coding Example Image From A Cloud Guru",
+      'src': "images/example1.png",
+      'comment':""
+    }
+  },
+  {
+    'title': "Publications",
+    'image': {
+      'desc': "Blurred Image of an Essay",
+      'src': "images/Research.jpg",
+      'comment':""
+    }
+  },
+  {
+    'title': "Miscellaneous",
+    'image': {
+      'desc': "K10 complete graph",
+      'src': "images/k10.png",
+      'comment':""
+    }
+  }
+]
+ReactDOM.render(<ExampleWork work = {myWork}/>, document.getElementById('example-work'));
